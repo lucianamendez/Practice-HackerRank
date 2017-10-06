3444072cceba808332620757812e52d57e680ae2
Luciana Mendez <dg.lucianamendez@gmail.com>
2017-10-06 16:24:34 -0300

    adding resolve

--- A02-function simpleArraySum.js
+++ A02-function simpleArraySum.js
@@ -1,8 +1,13 @@
 function simpleArraySum(n, ar) {
-   return (ar[0]+ar[1]+ar[2]+ar[3]+ar[4]+ar[5])
+   var sum = 0;		    		    
+   for (var i = 0; i < ar.length; i++) {
+   sum += ar[i];		    
+}	
+    return sum;
 }
 
 function main() {
+    
     var n = parseInt(readLine());
     ar = readLine().split(' ');
     ar = ar.map(Number);
