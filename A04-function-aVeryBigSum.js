7193f5ca814de1629fa8f6099e8df3b2ed000294
Luciana Mendez <dg.lucianamendez@gmail.com>
2017-10-06 17:30:47 -0300

    add resolve

---
+++ A04-function-aVeryBigSum.js
@@ -0,0 +1,16 @@
+function aVeryBigSum(n, ar) {
+    var sum=0;
+   for (var i=0 ; i< n ; i++){
+      sum += ar[i] 
+   }
+   return(sum);
+}
+
+function main() {
+    var n = parseInt(readLine());
+    ar = readLine().split(' ');
+    ar = ar.map(Number);
+    var result = aVeryBigSum(n, ar);
+    process.stdout.write("" + result + "\n");
+
+}
