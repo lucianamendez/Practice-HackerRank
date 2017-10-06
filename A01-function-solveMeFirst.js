41f6d946500c804711e2863e72b8bb86568cad63
Luciana Mendez <dg.lucianamendez@gmail.com>
2017-10-06 15:51:14 -0300

    adding resolve

---
+++ A01-function-solveMeFirst.js
@@ -0,0 +1,12 @@
+function solveMeFirst(a, b) {
+  return(a+b);  
+  
+}
+
+function main() {
+    var a = parseInt(readLine());
+    var b = parseInt(readLine());;
+
+    var res = solveMeFirst(a, b);
+    console.log(res);
+}
